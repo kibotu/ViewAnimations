88596fe292b7d0c44e92881f8045bec26abc8700
Jan Rabe <jan.rabe@wooga.net>
2013-12-09 11:18:56 +0100

    init

---
+++ src/sandbox/android/animation/SequentialActivity.java
@@ -0,0 +1,69 @@
+package sandbox.android.animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SequentialActivity extends Activity implements AnimationListener {
+
+	ImageView imgLogo;
+	Button btnStart;
+
+	// Animation
+	Animation animSequential;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sequential);
+
+		imgLogo = (ImageView) findViewById(R.id.imgLogo);
+		btnStart = (Button) findViewById(R.id.btnStart);
+
+		// load the animation
+		animSequential = AnimationUtils.loadAnimation(getApplicationContext(),
+				R.anim.sequential);
+
+		// set animation listener
+		animSequential.setAnimationListener(this);
+
+		// button click event
+		btnStart.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// start the animation
+				imgLogo.startAnimation(animSequential);
+			}
+		});
+
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		// Take any action after completing the animation
+
+		// check for zoom in animation
+		if (animation == animSequential) {
+		}
+
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sandbox/android/animation/TogetherActivity.java
@@ -0,0 +1,69 @@
+package sandbox.android.animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class TogetherActivity extends Activity implements AnimationListener {
+
+	ImageView imgLogo;
+	Button btnStart;
+
+	// Animation
+	Animation animTogether;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_together);
+
+		imgLogo = (ImageView) findViewById(R.id.imgLogo);
+		btnStart = (Button) findViewById(R.id.btnStart);
+
+		// load the animation
+		animTogether = AnimationUtils.loadAnimation(getApplicationContext(),
+				R.anim.together);
+
+		// set animation listener
+		animTogether.setAnimationListener(this);
+
+		// button click event
+		btnStart.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// start the animation
+				imgLogo.startAnimation(animTogether);
+			}
+		});
+
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		// Take any action after completing the animation
+
+		// check for zoom in animation
+		if (animation == animTogether) {
+		}
+
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
