7c3d954eba8eabc39101da8a178fae7cd3a188dd
kosmonautdnb <st.mader@gmx.de>
2013-12-10 08:14:00 -0800

    * animation

--- src/sandbox/android/animation/EXOAnimation.java
+++ src/sandbox/android/animation/EXOAnimation.java
@@ -74,6 +74,28 @@ class EXOAnimationCurveSin implements EXOAnimationCurveGetter
     }
 }
 
+class EXOAnimationCurveLinearInOut implements EXOAnimationCurveGetter
+{
+    double fadeintill   = 0.0;
+    double fadeoutfrom  = 1.0;
+
+    static EXOAnimationCurveLinearInOut create(double fadeintill,double fadeoutfrom)
+    {
+        EXOAnimationCurveLinearInOut ret = new EXOAnimationCurveLinearInOut();
+        ret.fadeintill  = fadeintill;
+        ret.fadeoutfrom = fadeoutfrom;
+        return ret;
+    }
+
+    @Override
+    public double getInfluence(double t) {
+        if (t < fadeintill)
+            return Math.max(0.0,t / fadeintill);
+        if (t > fadeoutfrom)
+            return Math.min(1.0,1.0 - (t - fadeoutfrom) / (1.0-fadeoutfrom));
+        return 1.0;
+    }
+}
 
 class EXOAnimationElement implements AnimStateGetter
 {
