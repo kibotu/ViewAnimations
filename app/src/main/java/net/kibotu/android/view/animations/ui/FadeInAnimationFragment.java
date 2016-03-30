1287338a2c1da8d6f99250dff5088b057196e5a3
Jan Rabe <cloudgazer3d@gmail.com>
2016-03-30 23:14:31 +0200

    moved more activity logic to fragments

--- app/src/main/java/net/kibotu/android/view/animations/ui/FadeInAnimationFragment.java
+++ app/src/main/java/net/kibotu/android/view/animations/ui/FadeInAnimationFragment.java
@@ -4,9 +4,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.Bind;
-import butterknife.OnItemClick;
+import butterknife.OnClick;
 import net.kibotu.android.view.animations.R;
+
+import static net.kibotu.android.view.animations.R.id.btnStart;
 
 /**
  * Created by Nyaruhodo on 30.03.2016.
@@ -23,13 +26,24 @@ public class FadeInAnimationFragment extends BaseFragment {
     protected void onViewCreated() {
 
         // load the animation
-        anim = AnimationUtils.loadAnimation(getContext(), R.anim.blink);
+        anim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
 
         // set animation listener
         anim.setAnimationListener(this);
     }
 
-    @OnItemClick(R.id.btnStart)
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+        // check for fade in animation
+        if (animation == anim) {
+            Toast.makeText(getContext(), "Animation Stopped", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @OnClick(btnStart)
     void onClick() {
         txtMessage.setVisibility(View.VISIBLE);
 
@@ -39,6 +53,6 @@ public class FadeInAnimationFragment extends BaseFragment {
 
     @Override
     public int getLayout() {
-        return R.layout.fragment_blink;
+        return R.layout.fragment_fade_in;
     }
 }
